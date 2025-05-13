05f9f586405b8af84f9eccc77abdd10361e82758
Darren Jordan <bingopilingo@gmail.com>
2025-05-13 14:35:48 +0100

    Fix: removed sync script from _document.tsx, moved SDK init to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://sdk.minepi.com/pi-sdk.js"
+        strategy="beforeInteractive"
+      />
+      <Script
+        id="pi-init"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `Pi.init({ version: "2.0", sandbox: true });`,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head />
       <body>
-        <script src="https://sdk.minepi.com/pi-sdk.js"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `Pi.init({ version: "2.0", sandbox: true });`,
-          }}
-        />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
